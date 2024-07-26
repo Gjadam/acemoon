5577586fba84318309afcba2d21914b6576f072a
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-07-26 19:19:50 +0330

    creating navbar

--- src/components/modules/navbar/Navbar.js
+++ src/components/modules/navbar/Navbar.js
@@ -3,33 +3,33 @@ import Image from 'next/image'
 // Icons
 import { FaShoppingBasket } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
+import Button from '../button/Button';
 
 export default function Navbar() {
     return (
-        <div className=' flex justify-between items-center px-10 py-5'>
+        <div className='container-fluid flex justify-between items-center py-5'>
             <div className="flex justify-center items-center gap-10">
                 <Image
                     src={"/images/png/logo.png"}
                     alt='logo'
                     height={0}
-                    width={250}
+                    width={170}
                 />
                 <div className=" flex gap-5">
-                    <span className=' font-bold hover:text-pink-500 cursor-pointer transition-colors'>صفحه اصلی</span>
-                    <span className=' font-bold hover:text-pink-500 cursor-pointer transition-colors'>فروشگاه</span>
-                    <span className=' font-bold hover:text-pink-500 cursor-pointer transition-colors'>درباره ما</span>
-                    <span className=' font-bold hover:text-pink-500 cursor-pointer transition-colors'>درباره من</span>
+                    <span className=' font-bold hover:text-rose-500 cursor-pointer transition-colors'>صفحه اصلی</span>
+                    <span className=' font-bold hover:text-rose-500 cursor-pointer transition-colors'>فروشگاه</span>
+                    <span className=' font-bold hover:text-rose-500 cursor-pointer transition-colors'>درباره ما</span>
+                    <span className=' font-bold hover:text-rose-500 cursor-pointer transition-colors'>درباره من</span>
                 </div>
             </div>
             <div className=" flex justify-center items-center gap-5">
-                <div className=" flex justify-center items-center gap-3">
-                    <input type="text" placeholder='جستوجو...' className=' focus:border-b-1 border-opacity-0 focus:border-opacity-100 border-pink-500 pb-1 transition-all placeholder:text-xs outline-none'/>
-                    <IoSearch />
+                <div className=" relative  ">
+                    <input type="text" placeholder='جستوجو...' className='p-2.5 rounded-3xl border-1 focus:border-rose-500  placeholder:text-xs outline-none transition-all' />
+                    <IoSearch className=' absolute left-3 top-3.5 text-lg' />
                 </div>
-                <div className="flex justify-center items-center gap-3 bg-pink-500 text-white w-32 p-3 rounded-3xl">
-                    <span className="">آیتم ها</span>
+                <Button text={"آیتم ها"} >
                     <FaShoppingBasket />
-                </div>
+                </Button>
             </div>
         </div>
     )
