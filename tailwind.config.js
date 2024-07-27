e9727d9e6a1ef966026245e906db562d93fe9ce2
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-07-27 22:00:04 +0330

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,40 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'secondary': '#1c1c1c',
+      },
       borderWidth: {
         '1': '1px'
       },
+      height: {
+        '120': "30rem"
+      },
+      width: {
+        '120': "30rem"
+      }
     },
   },
-  plugins: [],
+  plugins: [  
+    function ({ addUtilities }) {  
+      addUtilities({  
+        '.container-fluid': {  
+          width: '100%',  
+          maxWidth: '100%',  
+          paddingLeft: '5rem',  
+          paddingRight: '5rem',  
+          marginLeft: 'auto',  
+          marginRight: 'auto',  
+        },  
+        '.drag-none': {
+					'-webkit-user-drag': 'none',
+					'-khtml-user-drag': 'none',
+					'-moz-user-drag': 'none',
+					'-o-user-drag': 'none',
+					'user-drag': 'none'
+				}
+      });  
+    },  
+    
+  ],  
 };
