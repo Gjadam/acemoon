203c80701f6666b900b87fad9c3827bd498c369e
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-08-14 14:33:30 +0330

    configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['upcdn.io','lva4kcfubltuu0k7.public.blob.vercel-storage.com'],
+  },
+};
 
 export default nextConfig;
